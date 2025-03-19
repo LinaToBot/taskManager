7da80332953db930cdf2a909fd967df3e185601c
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-03-19 17:33:31 -0600

    feat(taskController): transformedTasks added

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,58 +1,17 @@
-// import Task from "../models/Task.js";
-
-// exports.getTasks = async (req, res) => {
-//   try {
-//     const tasks = await Task.find();
-//     res.json(tasks);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
-// exports.createTask = async (req, res) => {
-//   const task = new Task({
-//     title: req.body.title,
-//     description: req.body.description,
-//     status: "pending",
-//   });
-
-//   try {
-//     const newTask = await task.save();
-//     res.status(201).json(newTask);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
-
-// exports.updateTask = async (req, res) => {
-//   try {
-//     const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-//       new: true,
-//     });
-//     if (!task) return res.status(404).json({ message: "Task don´t find" });
-//     res.json(task);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
-
-// exports.deleteTask = async (req, res) => {
-//   try {
-//     const task = await Task.findByIdAndDelete(req.params.id);
-//     if (!task) return res.status(404).json({ message: "Task don´t find" });
-//     res.json({ message: "Task deleted" });
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
 import Task from "../models/Task.js";
 
 const taskController = {
   getTasks: async (req, res) => {
     try {
       const tasks = await Task.find();
-      res.json(tasks);
+
+      const transformedTasks = tasks.map((task) => ({
+        id: task._id.toString(),
+        title: task.title,
+        status: task.status,
+      }));
+
+      res.json(transformedTasks);
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
@@ -67,7 +26,7 @@ const taskController = {
 
     try {
       const newTask = await task.save();
-      res.status(201).json(newTask);
+      res.status(201).json({ id: newTask._id.toString(), ...newTask._doc });
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
@@ -79,7 +38,8 @@ const taskController = {
         new: true,
       });
       if (!task) return res.status(404).json({ message: "Task not found" });
-      res.json(task);
+
+      res.json({ id: task._id.toString(), ...task._doc });
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
@@ -89,6 +49,7 @@ const taskController = {
     try {
       const task = await Task.findByIdAndDelete(req.params.id);
       if (!task) return res.status(404).json({ message: "Task not found" });
+
       res.json({ message: "Task deleted" });
     } catch (err) {
       res.status(500).json({ message: err.message });
