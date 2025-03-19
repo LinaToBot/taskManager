6f1449ae01738ad39564080e2f808efdbab3a0ec
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-03-19 17:32:58 -0600

    feat(server) clean code

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,3 @@
-// dotenv.config();
-// dbConnection();
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/api/tasks", import("./routes/taskRoutes.js"));
-// app.use("/api/users", import("./routes/userRoutes.js"));
-
-// app.listen(5000, () => console.log("Server running on port 5000"))
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
