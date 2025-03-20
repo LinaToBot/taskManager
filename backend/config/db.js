903e3b77303a91bd6d74f66501c7da856a84e0c3
Paulina Ignacio Sanvicente <paulinaignacio@tuhabi.mx>
2025-03-19 22:17:08 -0600

    feat(backend): localhost replaced with Mongo Atlas URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("db MDB connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Conectado a MongoDB Atlas");
   } catch (error) {
-    console.log(error, "error");
+    console.error("❌ Error al conectar a MongoDB:", error.message);
     process.exit(1);
   }
 };
